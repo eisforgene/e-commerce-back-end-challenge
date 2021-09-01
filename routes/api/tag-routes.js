62bdf395aacb11b1240033d0a85410789b01e017
Bomb Atomically <ieugenelee@gmail.com>
2021-09-01 11:05:08 -0700

    finished api routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,61 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model:Product, through: ProductTag}]
+  }).then(function(data){
+    console.log('Find all tags', data),
+    res.json(data)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id:req.params.id
+    },
+    include: [{model:Product, through: ProductTag}]
+  }).then(function(data){
+    console.log('Find one tag', data),
+    res.json(data)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then(function(data){
+    console.log('New Tag', data),
+    res.json(data)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+  .then(function(data){
+    console.log('Updated Tag', data),
+    res.json(data)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then(function(data){
+    console.log('Deleted Tag', data),
+    res.json(data)
+  })
 });
 
 module.exports = router;
